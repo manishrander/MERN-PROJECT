e4afcad5b6c050a19c64d4a80eb11609ddaf3085
manishrander <manishrander68@gmail.com>
2024-10-03 22:54:58 +0530

    second commit

--- backend/app.js
+++ backend/app.js
@@ -19,6 +19,13 @@ app.use(
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "Come to India",
+    });
+});
+
 app.use("/api/v1/message", messageRouter);
 dbConnection();
 export default app;
